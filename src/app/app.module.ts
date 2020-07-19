5c4323f834590885ce0a496a8ed9aaee6359be60
sezalazar <sebastianformularios@gmail.com>
2020-07-18 22:29:00 -0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FeedbackComponent } from './Components/feedback/feedback.component';
+import { PricingTableComponent } from './Components/pricing-table/pricing-table.component';
+import { ClientsComponent } from './Components/clients/clients.component';
+import { ContentComponent } from './Components/content/content.component';
+import { GaleryComponent } from './Components/galery/galery.component';
+import { TechnologiesComponent } from './Components/technologies/technologies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeedbackComponent,
+    PricingTableComponent,
+    ClientsComponent,
+    ContentComponent,
+    GaleryComponent,
+    TechnologiesComponent
   ],
   imports: [
     BrowserModule
